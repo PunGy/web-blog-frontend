9ba89d9015ab213d0d06ea1ea802ba85105599fe
Maxim Iakovlev <max.yakovlev555@gmail.com>
2022-04-29 21:58:40 +0200

    replace post html with web component

---
+++ components/blog-post/blog-post.js
@@ -0,0 +1,15 @@
+class BlogPost extends HTMLElement {
+    constructor() {
+        super();
+        const template = document.getElementById('post-template')
+
+        const shadow = this.attachShadow({ mode: 'open' })
+        if (template != null) {
+            shadow.appendChild(template.content.cloneNode(true))
+        } else {
+            shadow.innerHTML = '<b style="color: white">Define blog-post template!</b>'
+        }
+    }
+}
+
+customElements.define('blog-post', BlogPost)
